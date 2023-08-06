ae32de296c8e5b9b9a38c969d83ad8e2be58aeac
Erik Unger <erik@erikunger.com>
2023-08-06 15:10:52 +0200

    added method FileProvider.ListFiles

--- fileprovider.go
+++ fileprovider.go
@@ -9,6 +9,7 @@ import (
 // FileProvider is an interface for read access to named files
 type FileProvider interface {
 	HasFile(filename string) (bool, error)
+	ListFiles(ctx context.Context) (filenames []string, err error)
 	ReadFile(ctx context.Context, filename string) ([]byte, error)
 }
 
@@ -25,25 +26,40 @@ func (d dirFileProvider) HasFile(filename string) (bool, error) {
 	return d.dir.Join(filename).Exists(), nil
 }
 
+func (d dirFileProvider) ListFiles(ctx context.Context) (filenames []string, err error) {
+	err = d.dir.ListDirContext(ctx, func(file fs.File) error {
+		filenames = append(filenames, file.Name())
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return filenames, nil
+}
+
 func (d dirFileProvider) ReadFile(ctx context.Context, filename string) ([]byte, error) {
 	return d.dir.Join(filename).ReadAllContext(ctx)
 }
 
-func MemFileProvider(file *fs.MemFile) FileProvider {
-	return memFileProvider{file}
-}
+// func MemFileProvider(file fs.MemFile) FileProvider {
+// 	return memFileProvider{file}
+// }
 
-type memFileProvider struct {
-	file *fs.MemFile
-}
+// type memFileProvider struct {
+// 	file fs.MemFile
+// }
 
-func (mem memFileProvider) HasFile(filename string) (bool, error) {
-	return mem.file.FileName == filename, nil
-}
+// func (mem memFileProvider) HasFile(filename string) (bool, error) {
+// 	return mem.file.FileName == filename, nil
+// }
 
-func (mem memFileProvider) ReadFile(ctx context.Context, filename string) ([]byte, error) {
-	if filename != mem.file.FileName {
-		return nil, fs.NewErrPathDoesNotExist(filename)
-	}
-	return mem.file.FileData, nil
-}
+// func (mem memFileProvider) ListFiles(ctx context.Context) (filenames []string, err error) {
+// 	return []string{mem.file.FileName}, nil
+// }
+
+// func (mem memFileProvider) ReadFile(ctx context.Context, filename string) ([]byte, error) {
+// 	if filename != mem.file.FileName {
+// 		return nil, fs.NewErrPathDoesNotExist(filename)
+// 	}
+// 	return mem.file.FileData, nil
+// }
